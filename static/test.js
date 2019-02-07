a0538c9b4a1a1675a5f71bbe12cc0cfa60a68dbf
Casper Søkol <38788839+j-heavy@users.noreply.github.com>
2019-02-07 20:27:10 +0500

    Update test.js

--- static/test.js
+++ static/test.js
@@ -1,36 +1,42 @@
 function getXmlHttp() {
-    let xmlhttp;
+    let xmlhttp
+    
     try {
-      xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+      xmlhttp = new ActiveXObject("Msxml2.XMLHTTP")
     } catch (e) {
-    try {
-      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    } catch (E) {
-      xmlhttp = false;
+        try {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")
+        } catch (E) {
+            xmlhttp = false
+        }
     }
+    
+    if (!xmlhttp && XMLHttpRequest === void 0) {
+      xmlhttp = new XMLHttpRequest()
     }
-    if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
-      xmlhttp = new XMLHttpRequest();
-    }
-    return xmlhttp;
-  }
-  function create() {
-    var name = document.getElementById("Name").value;
-    var ava = document.getElementById("Avatar").value;
-    var text = document.getElementById("Text").value;
-    var date = document.getElementById("Date").value;
-    var xmlhttp = getXmlHttp();
-    xmlhttp.open('POST', '/quotes', true);
-    xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xmlhttp.send("base64=true&name=" + encodeURIComponent(name) + "&ava=" + encodeURIComponent(ava) +"&text=" + encodeURIComponent(text) + "&date=" + encodeURIComponent(date));
+    
+    return xmlhttp
+}
+
+function create() {
+    const name = document.getElementById("Name").value
+    const ava = document.getElementById("Avatar").value
+    const text = document.getElementById("Text").value
+    const date = document.getElementById("Date").value
+
+    const xmlhttp = getXmlHttp()
+
+    xmlhttp.open('POST', '/quotes', true)
+    xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+    xmlhttp.send("base64=true&name=" + encodeURIComponent(name) + "&ava=" + encodeURIComponent(ava) +"&text=" + encodeURIComponent(text) + "&date=" + encodeURIComponent(date))
     xmlhttp.onreadystatechange = function() {
       if (xmlhttp.readyState == 4) {
-        if(xmlhttp.status == 200) {
+        if (xmlhttp.status == 200) {
             console.log('все ок')
             var result = ('data:image/png;base64,' + xmlhttp.responseText);
             document.getElementById('quoteimg').src =result;
             document.getElementById('downloadimg').href =result;
-        }
-      }
-    };
-  }
+       }
+     }
+   }
+}
